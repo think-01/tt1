6e8f586386111423592d01139bcb3a588fca2aab
slawek <slawek@t01.pl>
2022-12-13 19:06:51 +0100

    init

---
+++ src/Apps/ContextState/ContextState.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <smart-input
+        :valid="valid"
+        :pending="pending"
+        :disabled="disabled"
+
+        @blur="validate"
+        @toggle="toggle"
+
+        v-model="value"
+    />
+  </div>
+</template>
+
+<script>
+import SmartInput from "@/components/SmartInput";
+import inputService, { states, transistions } from "./input.service";
+
+
+export default {
+  components: {
+    SmartInput
+  },
+  data() {
+    return {
+      inputService
+    }
+  },
+  computed: {
+    value: {
+      get() {
+        return this.inputService.context
+      },
+      set(v) {
+        this.inputService.context = v
+      }
+    },
+    valid() {
+      return this.inputService.currentState !== states.errors
+    },
+    pending() {
+      return this.inputService.currentState === states.validating
+    },
+    disabled() {
+      return this.inputService.currentState === states.disabled
+    }
+  },
+  methods: {
+    validate() {
+      this.inputService.send('UPDATE')
+    },
+    toggle() {
+      this.inputService.send('TOGGLE')
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 import Vue from 'vue'
 Vue.config.productionTip = false
 
+import { inspect } from "@xstate/inspect";
+
+inspect({
+  iframe: false
+});
+
+//import App from './Apps/Trivia/Trivia'
+//import App from './Apps/Flags/Flags'
+//import App from './Apps/SimpleState/SimpleState'
+//import App from './Apps/ActionsState/ActionsState'
+//import App from './Apps/ContextState/ContextState'
+import App from './Apps/XState/XState'
+
 new Vue({
   el: '#app',
   components: {
-    //App
+    App
   },
   template: '<app/>'
 })
