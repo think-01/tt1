f1fcce10a34dc8d2ad8122d9adc35b8c71edd4c3
slawek <slawek@t01.pl>
2022-12-14 17:48:03 +0100

    vite

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 Vue.config.productionTip = false
 
+import App from './Apps/App.vue'
+
 new Vue({
   el: '#app',
   components: {
-    //App
+    App
   },
   template: '<app/>'
 })
